ade37337f1a2b82a6793fd2595dc126ff9b838a1
KevinVierhuis <kevin.vierhuis@student.aventus.nl>
2024-10-02 15:30:21 +0200

    Proberen een werkend navigation bar te krijgen

---
+++ app/app.tsx
@@ -0,0 +1,21 @@
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../Home';
+import Workouts from './Workouts';
+import Settings from './Settings';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Workouts" component={Workouts} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
